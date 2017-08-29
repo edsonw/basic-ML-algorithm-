52a660af87babbaeee327773dfb24d5c47f2b9ca
edsonw <969693577@qq.com>
2017-08-29 14:20:15 +0800

    first use

---
+++ gradient descent/Bisection Search.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/29 11:46
+# @Author  : sunday
+# @Site    : 
+# @File    : Bisection Search.py
+# @Software: PyCharm
---
+++ gradient descent/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/29 11:45
+# @Author  : sunday
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
