54ce32246165e960c3b7a3ac23f9a5bcbede78b7
edsonw <969693577@qq.com>
2017-08-29 22:06:46 +0800

    binary search with alpha
    一个使用二分搜索实现寻找最优学习率的算法

--- gradient descent/Bisection Search.py
+++ gradient descent/Bisection Search.py
@@ -5,3 +5,39 @@
 # @Site    : 
 # @File    : Bisection Search.py
 # @Software: PyCharm
+
+
+import numpy as np
+def bisection(dfun,theta,args,d,low,high,maxiter=1e4):
+    """
+    #funtionality:find the root of the function(fun) in the interval [low,high]
+    #使用二分搜索的方式查找最优学习率
+    :param dfun:compute the gradient of function f(x)
+    :param theta:Parameters of the model
+    :param args: other variavles needed ti compute the value of dfun
+    :param d:
+    :param [low,high]:the interval which contains the root
+    :param maxiter: the max number of iterations
+    :return:
+    """
+    eps=1e-6
+    val_low = np.sum(dfun(theta+low*d,args)*d.T)
+    val_high = np.sum(dfun(theta+high*d,args)*d.T)
+
+    if val_low*val_high>0:
+        raise  Exception('Invalid interval!')
+
+    inter_num=1
+    while inter_num<maxiter:
+        mid=(low+high)/2
+        val_mid=np.sum(dfun(theta+mid*d,args)*d.T)
+        if abs(val_mid<eps or abs(high-low)<eps):
+            return mid
+        elif val_mid*val_low>0:
+            low=mid
+        else:
+            high=mid
+        iter_num=iter_num+1
+
+    def loss_funtion(theta,args):
+        pass
